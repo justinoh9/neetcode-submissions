a732d092b00064eb4f5153dfb0a404f0fa176bd7
Justin Oh <ohjustin1009@gmail.com>
2026-08-16 21:32:57 -0400

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # first, clean the word
+        cleanString = ""
+        cleanStringReversed = ""
+        for char in s:
+            if 'a' <= char <= 'z' or '0' <= char <= '9':
+                cleanString = cleanString + char
+                cleanStringReversed = char + cleanStringReversed
+            elif 'A' <= char <= 'Z':
+                cleanString = cleanString + char.lower()
+                cleanStringReversed = char.lower() + cleanStringReversed
+        
+        if cleanString == cleanStringReversed:
+            return True
+        return False
