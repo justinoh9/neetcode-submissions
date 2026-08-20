21a17f238185228ae1fada9cfc9cc5992e584ad6
Justin Oh <ohjustin1009@gmail.com>
2026-08-20 16:53:15 -0400

    Add: validate-parentheses - submission-17

---
+++ Data Structures & Algorithms/validate-parentheses/submission-17.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        prio = []
+
+        # priority stack quesiton i think
+
+        # iirc, 4 cases
+
+        for i in range(len(s)):
+            if (s[i] == "(") or (s[i] == "[") or (s[i] == "{"):
+                prio.append(s[i])
+            elif not prio:
+                return False
+            elif (s[i] == ")" and prio[-1] != "(") or (s[i] == "]" and prio[-1] != "[") or (s[i] == "}" and prio[-1] != "{"): 
+                return False
+            # elif (s[i] == ")" and prio[-1] == "(") or (s[i] == "]" and prio[-1] == "[") or (s[i] == "}" and prio[-1] == "{"):
+            else: 
+                prio.pop()
+        return prio == []
