da40f18a2b006a4220dfe2a082d4dac7c1b6a37c
Justin Oh <ohjustin1009@gmail.com>
2026-08-29 17:02:06 -0400

    Add: top-k-elements-in-list - submission-8

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-8.py
@@ -0,0 +1,24 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+
+        # count how many times each number appears
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
+
+        # index = frequency
+        # value = numbers that appear that many times
+        buckets = [[] for _ in range(len(nums) + 1)]
+
+        for num, freq in count.items():
+            buckets[freq].append(num)
+
+        res = []
+
+        # start from highest frequency and move down
+        for freq in range(len(buckets) - 1, 0, -1):
+            for num in buckets[freq]:
+                res.append(num)
+
+                if len(res) == k:
+                    return res
